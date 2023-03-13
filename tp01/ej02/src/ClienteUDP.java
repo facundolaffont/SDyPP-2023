6be9996079bc28fc46ae7feb70474fb6bc8ef2dd
Facundo Laffont <facundolaffont@gmail.com>
2023-03-12 22:00:05 -0300

    Se agrega el cliente y server UDP del ej2

---
+++ tp01/ej02/src/ClienteUDP.java
@@ -0,0 +1,51 @@
+package tp01.ej02.src;
+
+import java.io.IOException;
+import java.net.*;
+
+public class ClienteUDP {
+
+    /* Miembros públicos */
+
+    public static void main(String[] args) {
+
+        try (
+            DatagramSocket socketUDP = new DatagramSocket() //  Crea el socket UDP que envía/recibe.
+        )
+        {
+            // Crea el mensaje.
+            String mensaje = "Hola, mundo!";
+            byte[] mensajeEnBytes = mensaje.getBytes();
+
+            // Genera el datagrama con el mensaje.
+            InetAddress address = InetAddress.getByName(HOST);
+            int puerto = PUERTO;
+            DatagramPacket paquete = new DatagramPacket(mensajeEnBytes, mensajeEnBytes.length, address, puerto);
+
+            // Envía el datagrama.
+            socketUDP.send(paquete);
+            System.out.println("Mensaje enviado al servidor: '" + mensaje + "'.");
+
+            // Queda escuchando que se devuelva el mensaje.
+            byte[] buffer = new byte[1024];
+            paquete = new DatagramPacket(buffer, buffer.length);
+            socketUDP.receive(paquete);
+
+            // Imprime el mensaje.
+            System.out.println(
+                "Mensaje recibido: '"
+                + new String(paquete.getData()) + "'."
+            );
+
+        } catch (IOException e) {
+            System.out.println( "Ocurrió un error: " + e.getMessage() );
+            e.printStackTrace();
+        }
+    }
+
+
+    /* Miembros privados */
+    
+    private static final int PUERTO = 5000;
+    private static final String HOST = "localhost";
+}
---
+++ tp01/ej02/src/ServidorUDP.java
@@ -0,0 +1,53 @@
+package tp01.ej02.src;
+
+import java.io.*;
+import java.net.*;
+
+public class ServidorUDP {
+    
+    /* Miembros públicos */
+
+    public static void main(String[] args) {
+
+        try (
+            DatagramSocket socketUDP = new DatagramSocket(PUERTO) // El server comienza a escuchar en un socket UDP.
+        ) {
+            System.out.println("Escuchando en puerto " + PUERTO + "...");
+
+            while (true) {
+
+                // Queda escuchando en el socket.
+                byte[] buffer = new byte[TAMANIO_BUFFER];
+                DatagramPacket paqueteRecibido = new DatagramPacket(buffer, buffer.length);
+                socketUDP.receive(paqueteRecibido);
+                
+                // Convierte el mensaje recibido en String y lo muestra.
+                String mensajeRecibido = new String( paqueteRecibido.getData(), 0, paqueteRecibido.getLength() );
+                System.out.println(
+                    "Nuevo mensaje recibido desde <"
+                    + paqueteRecibido.getAddress().getHostName() + ":"
+                    + paqueteRecibido.getPort() + ">: '"
+                    + mensajeRecibido + "'."
+                );
+
+                // Obtiene los datos del cliente y le devuelve el mensaje.
+                InetAddress direccionCliente = paqueteRecibido.getAddress();
+                int puertoCliente = paqueteRecibido.getPort();
+                byte[] mensajeEnBytes = mensajeRecibido.getBytes();
+                DatagramPacket respuesta = new DatagramPacket(mensajeEnBytes, mensajeEnBytes.length, direccionCliente, puertoCliente);
+                socketUDP.send(respuesta);
+            }
+
+        }
+        catch (IOException e) { 
+            System.out.println( "Ocurrió un error: " + e.getMessage() );
+            e.printStackTrace();
+        }
+    }
+
+
+    /* Miembros privados */
+
+    private static final int PUERTO = 5000;
+    private static final int TAMANIO_BUFFER = 1024;
+}
