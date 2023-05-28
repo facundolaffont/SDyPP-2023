be02511f8fa43aaa11169dc19c920ca526e0c008
franparzanese <parzanesefranco@gmail.com>
2023-05-28 16:39:54 -0300

    [TP2-EJ1] Ajustes en la desconexión del nodo extremo

--- tp02/ej1/extremo/src/main/resources/static/main.js
+++ tp02/ej1/extremo/src/main/resources/static/main.js
@@ -3,9 +3,10 @@ var disconnectBtn = document.getElementById("disconnect-btn");
 disconnectBtn.addEventListener("click", function() {
     if (confirm("¿Estás seguro que quieres desconectarte? Se cancelará cualquier"
             + " transferencia en curso.")) {
-        fetch("http://localhost:8080/disconnect").finally(function() {
-            document.getElementById("connected-panel").classList.add("d-none");
-            document.getElementById("disconnected-panel").classList.remove("d-none");
-        });
+        // Da aviso a los nodos maestros.
+        fetch("http://localhost:8080/disconnect");
+        // Muestra mensaje en el front.
+        document.getElementById("connected-panel").classList.add("d-none");
+        document.getElementById("disconnected-panel").classList.remove("d-none");
     }
 });
