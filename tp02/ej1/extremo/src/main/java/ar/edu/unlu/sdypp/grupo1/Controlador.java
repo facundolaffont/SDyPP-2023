33f7798eff7b5df8212f48b6676d95bc9667c838
franparzanese <parzanesefranco@gmail.com>
2023-06-13 01:14:42 -0300

    [TP2-EJ1] Agrega anotación para permitir CORS en los extremos

--- tp02/ej1/extremo/src/main/java/ar/edu/unlu/sdypp/grupo1/Controlador.java
+++ tp02/ej1/extremo/src/main/java/ar/edu/unlu/sdypp/grupo1/Controlador.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -67,6 +68,7 @@ public class Controlador {
      * @throws Exception Si se produce algún error al leer el archivo.
      */
     @GetMapping("/download")
+    @CrossOrigin(origins="*")
     public ResponseEntity<Resource> download(@RequestParam("name") String name) throws Exception {
         File file = this.extreme.getFile(name);
         Resource resource = new FileSystemResource(file);
